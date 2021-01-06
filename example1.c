9d21a8fb13dc2bca9e9b8ccd5bef50525dbd88aa
Emma Grampp <erik@grampp.se>
2021-01-06 14:28:47 +0100

    wrote prelim lexer

---
+++ example1.c
@@ -0,0 +1,4 @@
+int main() {
+	int q = 3;
+	return q;
+}
--- lexer.js
+++ lexer.js
@@ -1,101 +1,44 @@
-function Index() {
-		this.arr = []
-		this.len = 0
-		this.index = 0
-}
-Object.assign(
-		Index.prototype,
-		{
-				pop: function() {
-						return this.arr[this.index++]
-				},
-				peek: function() {
-						return this.arr[this.index]
-				},
-				retreat: function(n) {
-						this.index -= n
-				},
-				depleted: function() {
-						return this.index == this.len
-				}
-		})
-
-function IndexString(string) {
-		Index.call(this)
-		this.arr = string
-		this.len = string.length
-}
-IndexString.prototype = Object.create(Index.prototype)
-
-function IndexLex() {
-		Index.call(this)
-}
-IndexLex.prototype =
-		Object.assign(
-				Object.create(Index.prototype),
-				{
-						add: function(l) {
-								this.arr[len] = l
-								this.len++
-						}
-				})
-
-function lex(src, dest) {
-		for (let fun of [addNum, addPunct, addTxt]) {
-				if (fun(src, dest))
-						return true
+function addX(regex) {
+		return function(src, i, dest) {
+				regex.lastIndex = i
+				let m = src.match(regex)
+				if (m) {
+						dest[dest.length] = m[0]
+						return regex.lastIndex
+				} else return false
 		}
-		return false
 }
 
-function addNum(src, dest) {
-		let buf = ''
-		while ('0123456789'.includes(src.peek()))
-				buf += src.pop()
-		if (buf.length > 0) {
-				dest.add(buf)
-				return true
-		} else return false
-}
+const plausibleNum = /(0x)?[0-9]+(\.[0-9]*([Ee]-?[0-9]+)?)?[UuLl]?/y
+const addNum = addX(plausibleNum)
 
-const punctuators =
-			['[', ']', '(', ')', '{', '}', '.', '->', '++', '--', '&', '*',
-			 '+', '-', '~', '!', '/', '%', '<<', '>>', '<', '>', '<=', '>=',
-			 '==', '!=', '^', '|', '&&', '||', '?', ':', ';', '...', '=',
-			 '*=', '/=', '%=', '+=', '-=', '<<=', '>>=', '&=', '^=', '|=',
-			 ',', '#', '##', '<:', ':>', '<%', '%>', '%:', '%:%:']
-function addPunct(src, dest) {
-		let buf = ''
-		let c = 0
-		while ('[](){}.->+&*~!/%<=^|?:;#'.includes(src.peek())) {
-				buf += src.pop()
-				c++
-		}
-		if (buf.length > 0 && punctuators.includes(buf)) {
-				dest.add(buf)
-				return true
-		} else {
-				src.retreat(c)
+const punctuator = new RegExp(String.raw`((%:){1,2})|(\.\.\.)|(->)
+|([+\-<>&|#])\\5|([<>=!*/%+\-&^|]=)|([<>]{2}=)|(<[:%])|([:%]>)
+|([\[\](){}.&*+\-~!/%<>^|?:;=,#])`, 'y')
+const addPunct = addX(punctuator)
+
+const ident = /[_A-z][_A-z0-9]*/y
+const addId = addX(ident)
+
+const ws = /\s/y
+function eatWs(src, i) {
+		ws.lastIndex = i
+		if (src.match(ws))
+				return i+1
+		else
 				return false
-		}
 }
-
-const initChar = /[_A-z]/
-const txtChar = /[_A-z0-9]/
-function addTxt(src, dest) {
-		let buf = ''
-		if (!initChar.test(src.peek()))
-				return false
-		while (txtChar.test(src.peek()))
-				buf += src.pop()
-		dest.add(buf)
-		return true
-}
-
+						 
 function lexSource(src) {
-		let b1 = new IndexString(src)
-		let b2 = new IndexLex()
-		while (!b1.depleted())
-				lex(b1, b2)
-		return b2
+		let i = 0
+		let dest = []
+		lexLoop: while (i < src.length - 1)
+				for(fun of [eatWs, addNum, addPunct, addId]) {
+						let r = fun(src, i, dest)
+						if (r) {
+								i = r
+								continue lexLoop
+						}
+				}	
+		return dest
 }
