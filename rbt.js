1e57cf84c5b666662e76959ec5d47fc9cae53f6e
Emma Grampp <erik@grampp.se>
2021-02-03 13:58:21 +0100

    added iterator

--- rbt.js
+++ rbt.js
@@ -1,48 +1,66 @@
+function sibPar(n) {
+		if (!n.p || !n.p.p)	return null
+		else return n.p.p.l !== n.p ? n.p.p.l : n.p.p.r
+}
+
+function sib(n) {
+		if (!n.p) return null
+		else return n.p.l !== n ? n.p.l : n.p.r
+}
+
+function swap(n1, n2) {
+		if (n1.p) {
+				if (n1.p.l === n1)
+						n1.p.l = n2
+				else
+						n1.p.r = n2
+		}
+		[n1.p, n2.p] = [n2.p, n1.p]
+}
+
+function rotL(n) {
+		let a = n, b = n.r ? n.r.l : null, c = n.r;
+		swap(a, c);
+		a.r = b; b ? b.p = a : null
+		c.l = a; a.p = c
+}
+
+function rotR(n) {
+		let a = n.l, b = n.l ? n.l.r : null, c = n;
+		swap(c, a);
+		c.l = b; b ? b.p = c : null
+		a.r = c; c.p = a
+}
+
+function nullCh(n) {
+		return {p: n, l: null, r: null}
+}
+
+function* iter(root) {
+		if (root.r)
+				yield* iter(root.r)
+		yield root.v
+		if (root.l)
+				yield* iter(root.l)
+}
+
 class TreeSet {
 		constructor(comparator) {
-				this.root = {}
+				this.root = {p: null, l: null, r: null, red: false}
 				if (comparator instanceof Function)
 						this.comp = comparator
 				else
-						this.comp = (a, b) => a < b
-		}
-		
-		static sibPar(n) {
-				if (!n.p || !n.p.p)	return null
-				else return n.p.p.l !== n.p ? n.p.p.l : n.p.p.r
-		}
-
-		static sib(n) {
-				if (!n.p) return null
-				else return n.p.l !== n ? n.p.l : n.p.r
-		}
-
-		static swap(n1, n2) {
-				n1.p.l === n1 ? n1.p.l = n2 : n1.p.r = n2
-		}
-
-		static rotL(n) {
-				let a = n, b = n.r ? n.r.l : null, c = n.r;
-				swap(a, c)
-				a.r = b
-				c.l = a
-		}
-
-		static rotR(n) {
-				let a = n.l, b = n.l ? n.l.r : null, c = n;
-				swap(c, a)
-				c.l = b
-				a.r = c
+						this.comp = (a, b) => a - b
 		}
 
 		insertAdjust(n) {
 				n.red = true
 				if (n.p && n.p.red) {
-						if (sibPar(n) && sipPar(n).red) {
+						if (sibPar(n) && sibPar(n).red) {
 								n.p.red = false
 								sibPar(n).red = false
 								n.p.p.red = true
-								insertAdjust(n.p.p)
+								this.insertAdjust(n.p.p)
 						} else if (n.p.p && n.p.p.l === n.p) {
 								if (n.p.r === n)
 										rotL(n = n.p)
@@ -58,7 +76,7 @@ class TreeSet {
 						}
 				}
 				if (this.root.p) {
-						r = this.root
+						let r = this.root
 						while (r.p)
 								r = r.p
 						r.red = false
@@ -68,22 +86,26 @@ class TreeSet {
 		
 		insert(v, from) {
 				let n = from ? from : this.root
-				if (!base.v) {
-						base.v = v
-						insertAdjust(n)
+				if (!n.v) {
+						n.v = v
+						this.insertAdjust(n)
 				} else {
-						let com = this.comparator(base.v, v)
+						let com = this.comp(n.v, v)
 						if (com < 0) {
 								if (!n.l)
-										n.l = {p: n}
-								insert(v, n.l)
+										n.l = nullCh(n)
+								this.insert(v, n.l)
 						} else if (com > 0) {
 								if (!n.r)
-										n.r = {p: n}
-								insert(v, n.r)
+										n.r = nullCh(n)
+								this.insert(v, n.r)
 						} else 
 								throw new Error(
-										'Value ' + v +'compares 0 to ' + n.v + '!')
+										'Value ' + v + 'compares 0 to ' + n.v + '!')
 				}
+		}
+
+		[Symbol.iterator]() {
+				return iter(this.root)
 		}
 }
