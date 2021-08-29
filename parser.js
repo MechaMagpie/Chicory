8d3a22c94c6e41b9ef9e49df9682a9dd9ee8dcc5
MechaMagpie <emma.grampp@gmail.com>
2021-08-29 16:27:09 +0200

    uuuuuuu

--- parser.js
+++ parser.js
@@ -7,17 +7,17 @@ class Node {
 	}
 }
 
-function singleReg(regex, type) {
+function singleReg(type, regex) {
 	return function (buffer) {
 		let res = regex.match(buffer[0])
 		if (res && res[0].length == buffer[0].length) {
-			return [new type(val = buffer[0]), buffer.slice(1)]
+			return [new type({val: buffer[0]}), buffer.slice(1)]
 		}
 	}
 }
 
 function disjunction(funs) {
-	return function(buffer) {
+	return function (buffer) {
 		for (fun of funs) {
 			let res = fun(buffer)
 			if (res) return res
@@ -26,8 +26,44 @@ function disjunction(funs) {
 	}
 }
 
-function sequence(...terms) {
-	
+function multiSequence(type, isMult, ...terms) {
+	function consumePunct(punctuator, buffer) {
+		if (punctuator == buffer[0].text)
+			return buffer.slice(1)
+		else
+			return false
+	}
+	function parseTerm(term, buffer, result) {
+		let res, fun = (isMult ? term[1] : term)
+		if (!(res = fun(buffer))) return false
+		if (isMult) {
+			result[term[0]] = res[0]
+			return [result, res[1]]
+		} else
+			return [res[0], buffer]
+	}
+	return function (buffer) {
+		let result = {}
+		for (term of terms) {
+			if (typeof term == 'string')
+				if (!(buffer = consumePunct(term, buffer)))
+					return false
+			else {
+				let res1 = parseTerm(term, buffer, result)
+				if (!res1)
+					return false
+				[result, buffer] = res1				
+			}
+		}
+		if (isMult)
+			return [new type(result), buffer]
+		else
+			return [result, buffer]
+	}
+}
+
+function sequence(type, ...terms) {
+	return multiSequence(type, false, terms)
 }
 
 const identifier = /[_A-z][_0-9A-z]/
@@ -44,27 +80,34 @@ const parseIntConstant = disjunction([
 	parseHexConstant, parseDecConstant, parseOctConstant])
 
 class HexConstant extends IntConstant {}
-const parseHexConstant = singleReg(/0[xX][0-9A-Fa-f]+/, HexConstant)
+const parseHexConstant = singleReg(HexConstant, /0[xX][0-9A-Fa-f]+/)
 
 class DecConstant extends IntConstant {}
-const parseDecConstant = singleReg(/[1-9][0-9]*/, DecConstant)
+const parseDecConstant = singleReg(DecConstant, /[1-9][0-9]*/)
 
 class OctConstant extends IntConstant {}
-const parseOctConstant = singleReg(/0[0-9]+/, OctConstant)
+const parseOctConstant = singleReg(OctConstant, /0[0-9]+/)
 
 class FloatConstant extends Constant {}
 const parseFloatConstant = disjunction([parseFraction, parseExponential])
-const parseFraction = singleReg(/[0-9]*\.[0-9]+([eE][0-9]+)?/, FloatConstant)
-const parseExponential = singleReg(/[0-9]+[eE][0-9]+/, FloatConstant)
+const parseFraction = singleReg(FloatConstant, /[0-9]*\.[0-9]+([eE][0-9]+)?/)
+const parseExponential = singleReg(FloatConstant, /[0-9]+[eE][0-9]+/)
 
 class CharConstant extends Constant {}
-const parseCharConstant = singleReg(
-	/'([^\\']|(\\['"?\\abfnrtv]))'/, CharConstant)
+const parseCharConstant = singleReg(CharConstant,
+	/'([^\\']|(\\['"?\\abfnrtv]))'/)
 
 class EnumConstant extends Constant {}
-const parseEnumConstant = singleReg(identifier, EnumConstant)
+const parseEnumConstant = singleReg(EnumConstant, identifier)
 
 class StringLiteral extends Node {}
-const parseStringLiteral = singleReg(
-	/"(([^"\\]|(\\["?\\abfnrtv]))*)"/, StringLiteral)
+const parseStringLiteral = singleReg(StringLiteral,
+	/"(([^"\\]|(\\["?\\abfnrtv]))*)"/)
+
+class Identifier extends Node {}
+const parseIdentifier = singleReg(Identifier, identifier)
+
+class PrimaryExpression extends Expression {}
+const parsePrimaryExpression = disjunction([
+	parseIdentifier, parseConstant,	parseStringLiteral,
 
